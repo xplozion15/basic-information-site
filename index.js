3e792c160d2bf69ab35b26766f81586fdd522435
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-08-15 14:55:51 +0530

    feat: implement showing html files based on the req url using readfile method

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-import {createServer} from "node:http";
+import { createServer } from "node:http";
+import fs from "node:fs";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = "localhost";
+const port = 8080;
 
-const server = createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end("Hello World");
+const server = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  if (req.url == "/") {
+    fs.readFile("index.html", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
+    });
+  } else if (req.url == "/about") {
+    fs.readFile("about.html", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
+    });
+  } else if (req.url == "/contact-me") {
+    fs.readFile("contact-me.html", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
+    });
+  } else {
+    fs.readFile("404.html", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
+    });
+  }
 });
 
-server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
