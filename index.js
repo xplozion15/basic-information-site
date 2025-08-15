8028d56ae055417fd2b5c7da1c6dbe3bae3897fa
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-08-15 16:14:57 +0530

    fix : use strict equality check against the req.url instead of loose equality check

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 8080;
 const server = createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
 
-  if (req.url == "/") {
+  if (req.url === "/") {
     fs.readFile("index.html", "utf8", (err, data) => {
       if (err) {
         console.error(err);
@@ -15,7 +15,7 @@ const server = createServer((req, res) => {
       }
       res.end(data);
     });
-  } else if (req.url == "/about") {
+  } else if (req.url === "/about") {
     fs.readFile("about.html", "utf8", (err, data) => {
       if (err) {
         console.error(err);
@@ -23,7 +23,7 @@ const server = createServer((req, res) => {
       }
       res.end(data);
     });
-  } else if (req.url == "/contact-me") {
+  } else if (req.url === "/contact-me") {
     fs.readFile("contact-me.html", "utf8", (err, data) => {
       if (err) {
         console.error(err);
