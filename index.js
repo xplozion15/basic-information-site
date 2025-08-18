262c42bf7cbfa5ba09439f5c1de3d970ac8e092f
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-08-18 14:18:58 +0530

    chore : rework the project with express

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-import { createServer } from "node:http";
+import express from "express";
+const app = express();
 import fs from "node:fs";
 
 const hostname = "localhost";
 const port = 8080;
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
 
-  if (req.url === "/") {
-    fs.readFile("index.html", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.end(data);
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("about.html", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.end(data);
-    });
-  } else if (req.url === "/contact-me") {
-    fs.readFile("contact-me.html", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.end(data);
-    });
-  } else {
-    fs.readFile("404.html", "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.end(data);
-    });
-  }
+app.get("/", (req, res) => {
+  fs.readFile("index.html", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.end(data);
+  });
 });
 
-server.listen(port, hostname, () => {
+app.get("/about", (req, res) => {
+  fs.readFile("about.html", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.end(data);
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  fs.readFile("contact-me.html", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.end(data);
+  });
+});
+
+app.get("/", (req, res) => {
+  fs.readFile("404.html", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.end(data);
+  });
+});
+
+
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
